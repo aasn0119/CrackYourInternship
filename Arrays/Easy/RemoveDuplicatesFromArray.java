1874af0530f00eec10e75ddeeeb1d46d57882839
aasn0119 <abhitrivedi119@gmail.com>
2024-07-15 08:39:47 +0530

    first question

---
+++ Arrays/Easy/RemoveDuplicatesFromArray.java
@@ -0,0 +1,31 @@
+package Arrays.Easy;
+
+
+public class RemoveDuplicatesFromArray {
+
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        
+        return i + 1;
+    }
+    
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2, 2, 3, 4, 4, 5};
+        int length = removeDuplicates(nums);
+        
+        System.out.println("Array after removing duplicates:");
+        for (int i = 0; i < length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
